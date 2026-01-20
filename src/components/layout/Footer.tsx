b4f4f5f179007c3d8f4a12c8b4714ca28b865142
Chidi Mathaias <chidimathayas@gmail.com>
2026-01-20 15:40:14 +0100

    Added more styleing to the nav bar and also created Footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-[100dvh] bg-brand-navy">
+      <div className="max-w-[90rem]">
+        <p>Footer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,26 +1,41 @@
 import Image from "next/image";
 import React from "react";
 import primaryLogo from "../../../public/TGIPrimaryLogo.svg";
+import cbnLogo from "../../../public/cbnLogo.svg";
+
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
     <div className="w-full py-5">
       <div className="max-w-[90rem] mx-auto flex justify-between items-center">
-        <Image src={primaryLogo} alt="TGI Pay Logo" width={120} height={40} />
-
-        <div className="flex justify-between items-center gap-10 font-light">
-          <p>Home</p>
-          <p>Who We Are</p>
-          <p>Our Product</p>
-          <p>Our Pricing</p>
-          <p>Contact Us</p>
+        <div className="flex flex-col items-end justify-end">
+          <Image src={primaryLogo} alt="TGI Pay Logo" width={120} height={40} />
+          <div className="flex items-center justify-center gap-1">
+            <p className="text-[0.7rem]">Licenced by CBN</p>
+            <Image src={cbnLogo} alt="TGI Pay Logo" width={10} height={10} />
+          </div>
         </div>
-        <div className="flex justify-between items-center gap-7">
+
+        <div className="flex items-center justify-between gap-10 font-normal">
+          <Link href="/">Home</Link>
+          <Link href="/whoWeAre">Who We Are</Link>
+          <Link href="/ourProduct">Our Product</Link>
+          <Link href="/ourPricing">Our Pricing</Link>
+          <Link href="/contactUs">Contact Us</Link>
+        </div>
+        <div className="flex items-center justify-between gap-7">
           <p>Pay your Task</p>
 
-          <Button>Login</Button>
-          <Button>Start free account</Button>
+          <Button className="rounded-lg">Login</Button>
+          <Button
+            size="lg"
+            variant="outline"
+            className="border-2 rounded-lg border-brand-navy text-brand-navy hover:bg-brand-navy/10"
+          >
+            Start free account
+          </Button>
         </div>
       </div>
     </div>
