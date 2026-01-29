66e75455d978b595c8f1e0bb8bf2aca8dcdaad93
Chidi Mathaias <chidimathayas@gmail.com>
2026-01-29 16:23:47 +0100

    Added ripple animation

--- src/components/sections/Home/ReadyToTransform.tsx
+++ src/components/sections/Home/ReadyToTransform.tsx
@@ -1,16 +1,53 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 import { Button } from "@/components/ui/button";
 
 import mobilephone from "../../../../public/mobilephone.svg";
 import FadeInAnimation from "@/components/ui/FadeInAnimation";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 const ReadyToTransform = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.from(".ripple-ring", {
+        x: 150,
+        scale: 0.5,
+        opacity: 0,
+        duration: 1.5,
+        stagger: 0.15,
+        ease: "elastic.out(1, 0.6)",
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top 60%",
+          toggleActions: "play none none reverse",
+        },
+      });
+    },
+    { scope: containerRef },
+  );
+
   return (
-    <div className="w-full lg:py-0 lg:pt-36 bg-brand-cyan">
+    <div
+      ref={containerRef}
+      className="relative w-full lg:py-0 lg:pt-36 bg-brand-cyan overflow-hidden"
+    >
+      <div className="absolute right-20 z-0  -translate-y-1/2 pointer-events-none top-[25%]">
+        <div className="relative flex items-center justify-center">
+          <div className="ripple-ring absolute w-[25rem] h-[25rem] border-2 border-gray-200/20 rounded-full shadow-lg" />
+          <div className="ripple-ring absolute w-[45rem] h-[45rem] border-[3px] border-gray-200/20 rounded-full shadow-lg" />
+          <div className="ripple-ring absolute w-[65rem] h-[65rem] border-[4.7px] border-gray-200/20 rounded-full shadow-lg" />
+        </div>
+      </div>
+
       <div className="max-w-[90rem] px-4 mx-auto ">
         <div className="flex flex-col-reverse items-center justify-center w-full gap-10 md:flex-row">
           {/* Card Image */}
