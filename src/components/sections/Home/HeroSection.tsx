326816339ed05812f07f630acfb66dc0e944be26
Chidi Mathaias <chidimathayas@gmail.com>
2026-03-23 14:44:44 +0100

    fixed hero text

--- src/components/sections/Home/HeroSection.tsx
+++ src/components/sections/Home/HeroSection.tsx
@@ -17,10 +17,10 @@ const HeroSection = () => {
   return (
     <section className="w-full lg:max-h-[60rem] h-[100dvh] bg-[#F9FAFB] ">
       <div className="max-w-[90rem] h-full flex justify-between items-center px-4 mx-auto overflow-y-hidden ">
-        <div className="flex flex-col items-center justify-center w-full h-full lg:flex-row md:gap-y-3 gap-y-10">
+        <div className="flex flex-col items-center justify-center w-full h-full lg:flex-row md:gap-y-3 gap-y-7">
           {/* left content*/}
           <div className=" w-full lg:w-1/2 lg:pt-[0rem] md:pt-[6rem] pt-[7rem] lg:space-y-5 md:space-y-2 space-y-3 ">
-            <h1 className="font-bold text-[30px] md:text-[50px] lg:text-[40px] xl:text-[55px] leading-tight lg:leading-[30px] xl:leading-[60px]">
+            <h1 className="font-bold text-[25px] md:text-[50px] lg:text-[40px] xl:text-[55px] leading-tight lg:leading-[30px] xl:leading-[60px]">
               <span className="block text-hero-navy">
                 {" "}
                 <SplitText
